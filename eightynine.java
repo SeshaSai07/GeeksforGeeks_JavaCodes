b403d31240d0e334eff1d20bd580e07b46e05fb5
Lakshmi Sesha Sai Maddineni <159765297+SeshaSai07@users.noreply.github.com>
2025-08-25 17:49:37 +0530

    Add files via upload

---
+++ eightynine.java
@@ -0,0 +1,23 @@
+public class eightynine 
+{
+    public static void main(String[] args) 
+    {
+        // multidimensional array declarartion
+        int [][] arr;
+
+        // intializing the row and column respectively
+        arr = new int[1][3];
+
+        // intializing the values
+        arr[0][0] = 3;
+        arr[0][1] = 4;
+        arr[0][2] = 5;
+
+        // displaying the values using index
+        System.out.println("arr[0][0] = " + arr[0][0]);
+        System.out.println("arr[0][1] = " + arr[0][1]);
+        System.out.println("arr[0][2] = " + arr[0][2]);
+        
+    }
+    
+}
